3d72a581ef5969bb0fd492abd4739a0c1a1283b6
Hieunk <kimhieu1989@gmail.com>
2021-05-15 16:31:21 +0700

    ADD ALL FILE

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNjHMehyOlDVINiUYM_oTYJQbvCm6B9LQ",
+    authDomain: "clone-6ab92.firebaseapp.com",
+    databaseURL: "https://clone-6ab92.firebaseio.com",
+    projectId: "clone-6ab92",
+    storageBucket: "clone-6ab92.appspot.com",
+    messagingSenderId: "1088806808796",
+    appId: "1:1088806808796:web:684b8952faee6656ad5819",
+    measurementId: "G-QY44ZX6T1E"
+  };
